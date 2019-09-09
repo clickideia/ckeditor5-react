8f05d93b6dc74f0718d544fdcdcf4f6907022d06
Arthur Cantarela <arthur.cantarela@clickideia.com.br>
2019-09-09 17:03:05 -0300

    Enabling new htmlTags instead of div: WIP - only h2

--- src/ckeditor.jsx
+++ src/ckeditor.jsx
@@ -3,12 +3,12 @@
  * For licensing, see LICENSE.md.
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 export default class CKEditor extends React.Component {
-	constructor( props ) {
-		super( props );
+	constructor(props) {
+		super(props);
 
 		// After mounting the editor, the variable will contain a reference to the created editor.
 		// @see: https://ckeditor.com/docs/ckeditor5/latest/api/module_core_editor_editor-Editor.html
@@ -17,16 +17,16 @@ export default class CKEditor extends React.Component {
 	}
 
 	// This component should never be updated by React itself.
-	shouldComponentUpdate( nextProps ) {
-		if ( !this.editor ) {
+	shouldComponentUpdate(nextProps) {
+		if (!this.editor) {
 			return false;
 		}
 
-		if ( this._shouldUpdateContent( nextProps ) ) {
-			this.editor.setData( nextProps.data );
+		if (this._shouldUpdateContent(nextProps)) {
+			this.editor.setData(nextProps.data);
 		}
 
-		if ( 'disabled' in nextProps ) {
+		if ("disabled" in nextProps) {
 			this.editor.isReadOnly = nextProps.disabled;
 		}
 
@@ -48,74 +48,84 @@ export default class CKEditor extends React.Component {
 		// We need to inject initial data to the container where the editable will be enabled. Using `editor.setData()`
 		// is a bad practice because it initializes the data after every new connection (in case of collaboration usage).
 		// It leads to reset the entire content. See: #68
-		return (
-			<div ref={ this.domContainer } dangerouslySetInnerHTML={ { __html: this.props.data || '' } }></div>
-		);
+		const propsToForward = {
+			ref: this.domContainer,
+			dangerouslySetInnerHTML: { __html: this.props.data || "" }
+		};
+
+		switch (this.props.htmlTag) {
+			case "h2": {
+				// eslint-disable-next-line jsx-a11y/heading-has-content
+				return <h2 {...propsToForward} />;
+			}
+			default: {
+				return <div {...propsToForward} />;
+			}
+		}
 	}
 
 	_initializeEditor() {
 		this.props.editor
-			.create( this.domContainer.current , this.props.config )
-			.then( editor => {
+			.create(this.domContainer.current, this.props.config)
+			.then(editor => {
 				this.editor = editor;
 
-				if ( 'disabled' in this.props ) {
+				if ("disabled" in this.props) {
 					editor.isReadOnly = this.props.disabled;
 				}
 
-				if ( this.props.onInit ) {
-					this.props.onInit( editor );
+				if (this.props.onInit) {
+					this.props.onInit(editor);
 				}
 
 				const modelDocument = editor.model.document;
 				const viewDocument = editor.editing.view.document;
 
-				modelDocument.on( 'change:data', event => {
+				modelDocument.on("change:data", event => {
 					/* istanbul ignore else */
-					if ( this.props.onChange ) {
-						this.props.onChange( event, editor );
+					if (this.props.onChange) {
+						this.props.onChange(event, editor);
 					}
-				} );
+				});
 
-				viewDocument.on( 'focus', event => {
+				viewDocument.on("focus", event => {
 					/* istanbul ignore else */
-					if ( this.props.onFocus ) {
-						this.props.onFocus( event, editor );
+					if (this.props.onFocus) {
+						this.props.onFocus(event, editor);
 					}
-				} );
+				});
 
-				viewDocument.on( 'blur', event => {
+				viewDocument.on("blur", event => {
 					/* istanbul ignore else */
-					if ( this.props.onBlur ) {
-						this.props.onBlur( event, editor );
+					if (this.props.onBlur) {
+						this.props.onBlur(event, editor);
 					}
-				} );
-			} )
-			.catch( error => {
-				console.error( error );
-			} );
+				});
+			})
+			.catch(error => {
+				console.error(error);
+			});
 	}
 
 	_destroyEditor() {
-		if ( this.editor ) {
-			this.editor.destroy()
-				.then( () => {
-					this.editor = null;
-				} );
+		if (this.editor) {
+			this.editor.destroy().then(() => {
+				this.editor = null;
+			});
 		}
 	}
 
-	_shouldUpdateContent( nextProps ) {
+	_shouldUpdateContent(nextProps) {
 		// Check whether `nextProps.data` is equal to `this.props.data` is required if somebody defined the `#data`
 		// property as a static string and updated a state of component when the editor's content has been changed.
 		// If we avoid checking those properties, the editor's content will back to the initial value because
 		// the state has been changed and React will call this method.
-		if ( this.props.data === nextProps.data ) {
+		if (this.props.data === nextProps.data) {
 			return false;
 		}
 
 		// We should not change data if the editor's content is equal to the `#data` property.
-		if ( this.editor.getData() === nextProps.data ) {
+		if (this.editor.getData() === nextProps.data) {
 			return false;
 		}
 
@@ -139,4 +149,3 @@ CKEditor.propTypes = {
 CKEditor.defaultProps = {
 	config: {}
 };
-
